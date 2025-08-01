c7a1f0d9583d83483f1c828b42a380c07841b346
Xiang <shelldry325@gmail.com>
2025-08-02 10:03:23 +1200

    factorial function

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# main.py
 
-print("Hello, World!")
+num_input = input("Enter a non-negative integer: ")
+
+
+try:
+    num = int(num_input)
+
+    if num < 0:
+        print("Error: Factorial is not defined for negative numbers.")
+    else:
+        factorial = 1
+        n = num
+        while n > 1:
+            factorial *= n
+            n -= 1
+
+        print(f"The factorial of {num} is: {factorial}")
+
+except ValueError:
+    print("Invalid input: Please enter an integer.")

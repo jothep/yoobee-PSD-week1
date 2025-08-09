44828884593281290921a23680b3c758efa8b2be
Xiang <shelldry325@gmail.com>
2025-08-10 11:46:21 +1200

    some fix

--- week02_practice04.py
+++ week02_practice04.py
@@ -2,19 +2,20 @@ class StringManipulator:
 
 #Using __init__ can easily initialize, define internal variables, and quickly call internal variables when using methods
 
-    def find_character(text, char):
+    def find_character(self, text, char):
         return text.find(char)
     
-    def get_length(text):
+    def get_length(self, text):
         return len(text)
 
-    def set_String(text):
+    def set_String(self, text):
         return text.upper()
 
+name = StringManipulator()
 string1 = "example"
-result = StringManipulator.find_character(text=string1, char="x")
-length = StringManipulator.get_length(text=string1)
-result_Upper = StringManipulator.set_String(text=string1)
+result = name.find_character(text=string1, char="x")
+length = name.get_length(text=string1)
+result_Upper = name.set_String(text=string1)
 
 print(result)
 print(length)

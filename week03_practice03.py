742a1f152c1682a872ac2c847f2a0df35f59dbef
Xiang <shelldry325@gmail.com>
2025-08-16 09:49:49 +1200

    update week03_practice02

--- week03_practice03.py
+++ week03_practice03.py
@@ -2,21 +2,22 @@ class process_data:
     def __init__(self, file_path):
         self.file_path = file_path
         
-    def print_data(self):
-        self.data = open(self.file_path, "r+")
-        self.data.seek(0)
-        lines = self.data.readlines()
-        for line in lines:
+    def print_data(self, start_line: int = 0, end_line: int = -1):
+        data = open(self.file_path, "r+")
+        data.seek(0)
+        lines = data.readlines()
+        for line in lines[start_line:end_line]:
             print(line[0:-1])
-        self.data.close()
+        data.close()
 
     def write_data(self, info):
-        self.data = open(self.file_path, "r+")
         self.data.write(info)
+
+    def close_file(self):
         self.data.close()
 
-    def count_words(self) -> int:
-        with open(self.file_path, "r") as f:
+    def count_words(file_path) -> int:
+        with open(file_path, "r") as f:
             data = f.read()
             words = data.split()
             f.close()
@@ -24,5 +25,6 @@ class process_data:
             return len(words)
 
 if __name__ == "__main__":
+    #process_data.count_words("demo_file.txt")
     pd = process_data("demo_file.txt")
-    pd.count_words()
+    pd.print_data()

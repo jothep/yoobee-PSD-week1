f3b8bd293e148ff9599e0aeeb52d685cd7b1a42a
Xiang <shelldry325@gmail.com>
2025-08-16 20:51:43 +1200

    week03 activity03

--- week03_practice03.py
+++ week03_practice03.py
@@ -1,30 +1,44 @@
-class process_data:
+import pandas as pd
+
+class convertDatasets:
     def __init__(self, file_path):
         self.file_path = file_path
         
-    def print_data(self, start_line: int = 0, end_line: int = -1):
-        data = open(self.file_path, "r+")
-        data.seek(0)
-        lines = data.readlines()
-        for line in lines[start_line:end_line]:
-            print(line[0:-1])
-        data.close()
+    def load_df(self, cols) -> pd.DataFrame:
+        df = pd.read_csv(self.file_path, header=None, names=cols)
+        for c in cols:
+            df[c] = df[c].astype("category")
+        return df
 
-    def write_data(self, info):
-        self.data.write(info)
+    def stats(self, df: pd.DataFrame,  strict: bool = False) -> pd.DataFrame:
+        before_na = df.isna().sum()
+        num = df.apply(pd.to_numeric, errors="coerce")
+        after_na = num.isna().sum()
+        newly_na = (after_na - before_na)
+        bad = newly_na[newly_na> 0]
+        if not bad.empty:
+            msg = "unconvertible values appeared in below cols, were set to 'NaN:' \n" + bad.to_string()
+            if strict:
+                raise ValueError(msg)
+            else:
+                print("warn!", msg)
 
-    def close_file(self):
-        self.data.close()
 
-    def count_words(file_path) -> int:
-        with open(file_path, "r") as f:
-            data = f.read()
-            words = data.split()
-            f.close()
-            print(len(words))
-            return len(words)
+        base = num.agg(["max", "min", "mean"]).T
+        abs_stats = num.abs().agg(["max", "min", "mean"]).T.add_prefix("absolute_")
+        out = base.join(abs_stats)
+        out = out.drop(index="class", errors="ignore")
+
+        print(out.to_string(float_format=lambda x: f"{x:.2f}" if pd.notna(x) else "NaN"))
+        return out
+
+def main():
+    cols = ["sepal length", "sepal width", "petal length", "petal width", "class"]
+    cd = convertDatasets("iris.data")
+    df_parquet = cd.load_df(cols)
+    cd.stats(df_parquet)
+    return 0
 
 if __name__ == "__main__":
-    #process_data.count_words("demo_file.txt")
-    pd = process_data("demo_file.txt")
-    pd.print_data()
+    main()
+    
